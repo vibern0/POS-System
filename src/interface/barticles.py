21124f73abfe40e6c558fa676783ed28f61ab9e5
BernardoVieira <bernardo5@live.com.pt>
2016-10-10 15:18:41 +0100

    PreparingReorganization
    New classes createt but not implemented yet!

---
+++ src/interface/barticles.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import kivy
+kivy.require('1.0.5')
+
+class bArticles:
+
+    def __init__(self):
+        pass
+
+    def updateListArticles(self, articles_list):
+        pass
