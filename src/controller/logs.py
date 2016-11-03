684a862055a3330d529dbb6f25189297f5f9a348
Bernardo Vieira <bernardo5@live.com.pt>
2016-11-03 15:31:00 +0000

    SolveBugs&Reorganize
    - Some bugs solved - Reorganize code

---
+++ src/controller/logs.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import kivy
+kivy.require('1.0.5')
+
+class Logs:
+
+    def login(username):
+        pass
+
+    def logout(username):
+        pass
+
+    def finishList(username):
+        pass
